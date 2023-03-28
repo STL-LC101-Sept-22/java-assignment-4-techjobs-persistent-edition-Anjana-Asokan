7d13daf05b01b2534422b0413b0b0823cfa592c9
Anjana Asokan <anjanaasokan@Anjanas-MacBook-Air.local>
2023-03-28 16:11:26 +0530

    completed taask 1

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull
+    @NotBlank
+    @Size(min = 1)
+    private String location;
 
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
 }
