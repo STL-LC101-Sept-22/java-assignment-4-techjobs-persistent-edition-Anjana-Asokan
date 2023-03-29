94fd01d686b86fa926fe6fecdec7eb3d2be54c04
Anjana Asokan <anjanaasokan@Anjanas-MacBook-Air.local>
2023-03-30 04:50:46 +0530

    completed task three

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,10 +1,15 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
     @NotNull
@@ -12,6 +17,9 @@ public class Employer extends AbstractEntity {
     @Size(min = 1)
     private String location;
 
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    List<Job> jobs = new ArrayList<Job>();
     public String getLocation() {
         return location;
     }
@@ -21,4 +29,5 @@ public class Employer extends AbstractEntity {
     }
 
     public Employer() {}
+
 }
